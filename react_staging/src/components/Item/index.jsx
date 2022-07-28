fc6b21986edf034e953d2f485d2e987229cb673c
Bella <896848118@qq.com>
2022-07-28 22:34:31 +0800

    add todo

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import "./index.css"
 export default class Item extends Component {
+
+  
+
+  // 标识鼠标移入，移出的回调
+  state = {mouse:false} 
+
+  // 写成高阶函数
+  handleMouse = (flag) =>{
+    return ()=>{
+      this.setState({mouse:flag})
+    }
+  }
+
+  // 勾选，取消勾选某一个todo的回调
+  handleCheck = (id)=>{
+    return (event)=>{
+      this.props.updateTodo(id,event.target.checked)
+    }
+  }
+
+ 
+
   render() {
     const {id,name,done} = this.props
+    const {mouse} = this.state
     return (
-      <li>
+      <li style={{backgroundColor:mouse?"#ddd":"#fff"}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)}/>
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:"none"}}>删除</button>
+        <button className="btn btn-danger" style={{display:mouse?"block":"none"}}>删除</button>
       </li>
     )
   }
