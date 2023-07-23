450112b922d990e7bef2ed9c44b85dd919b502c2
gl <gaolu@ubinavi.com.cn>
2023-07-23 14:44:55 +0800

    redux practice project

---
+++ redux_practice/src/components/Count/Count.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+  render() {
+    return (
+      <div>
+        <h1>当前求和为: 0</h1>
+        <select ref={c=>this.selectNumber = c}>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+        </select>
+        <br/>
+        <br/>
+        <button>+</button>
+        <button>-</button>
+        <button>当前求和为奇数再加</button>
+        <button>异步加</button>
+      </div>
+    )
+  }
+}
