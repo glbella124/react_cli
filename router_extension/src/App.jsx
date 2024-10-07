afaee6d3e84d947650822f8583e8e35a19c64230
Bella <896848118@qq.com>
2024-10-07 18:52:20 +0800

    feat: router_extension

---
+++ router_extension/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">React Router Demo</div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group"></div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
